5a06bdcf3312c762791704b8472d152f02dba649
Cross <Andy.Cross@mbakerintl.com>
2024-03-08 15:00:24 -0700

    add unit test for flow direction, black formatting and pylint test complete

--- src/overflow/generate_flow_direction_raster.py
+++ src/overflow/generate_flow_direction_raster.py
@@ -2,102 +2,106 @@ import math
 import numpy as np
 from numba import njit, prange
 from osgeo import gdal
-from util.raster import raster_chunker
-from breach_single_cell_pits import breach_single_cell_pits_in_chunk
-
+from .util.raster import raster_chunker
 
 
 @njit(parallel=True)
-def generate_flow_direction_raster(chunk,cell_size,nodata_value)-> tuple[np.ndarray,np.ndarray] :
+def generate_flow_direction_raster(
+    chunk, cell_size, nodata_value
+) -> tuple[np.ndarray, np.ndarray]:
     """
     This function is used to calculate flow direction in a chunk of a DEM.
     The function takes a chunk of a DEM as input and returns a chunk of DEM with flow direction values.
 
     Parameters
     ----------
-    chunk : np.ndarray
-        A chunk of a DEM.
+    chunk (np.ndarray) : Digital Elevation Model (DEM) chunk.
 
     Returns
     -------
     np.ndarray
         A chunk of a DEM with flow direction values.
     """
-    d8_directions_dict={0:128,1:1,2:2,3:4,4:8,5:16,6:32,7:64}
-    dx=[1,1,1,0,-1,-1,-1,0]
-    dy=[-1,0,1,1,1,0,-1,-1]
+    d8_directions_dict = {0: 128, 1: 1, 2: 2, 3: 4, 4: 8, 5: 16, 6: 32, 7: 64}
+    dx = [1, 1, 1, 0, -1, -1, -1, 0]
+    dy = [-1, 0, 1, 1, 1, 0, -1, -1]
 
     chunk_copy = chunk.copy()
     # Get the shape of the chunk
     rows, cols = chunk.shape
     # Loop through each cell in the chunk
-    
+
     # pylint: disable=not-an-iterable
-    for row in prange(2,rows-2):
-        for col in range(2,cols-2):
-            z=chunk[row,col]
+    for row in prange(2, rows - 2):
+        for col in range(2, cols - 2):
+            z = chunk[row, col]
+            slopes = []
             if z != nodata_value:
-                slopes=[]
                 for k in range(8):
-                    if chunk[row+dy[k],col+dx[k]] == nodata_value:
+                    # Check if the neighbor is a nodata value, used to keep array index constant
+                    if chunk[row + dy[k], col + dx[k]] == nodata_value:
                         slopes.append(-9999)
                     else:
-                        if k in [1,3,5,7]:
-                            slopes.append((z - chunk[row+dy[k],col+dx[k]])/cell_size)
+                        if k in [1, 3, 5, 7]:
+                            # Calculate the slope between the cell and its non-diagonal neighbors
+                            slope_calc = (
+                                z - chunk[row + dy[k], col + dx[k]]
+                            ) / cell_size
+                            slopes.append(slope_calc)
                         else:
-                            slopes.append((z - chunk[row+dy[k],col+dx[k]])/math.sqrt(cell_size**2+cell_size**2))
+                            # Calculate the slope between the cell and its diagonal neighbors
+                            slope_calc = (z - chunk[row + dy[k], col + dx[k]]) / (
+                                math.sqrt(2) * cell_size
+                            )
+                            slopes.append(slope_calc)
+
             if z != nodata_value:
+                # Check if all slopes are the same, if so set the flow direction to 255
+                all_same = True
                 for slope in slopes:
                     if slope != slopes[0]:
-                        m=max(slopes)
-                        if m < 0:
-                            chunk_copy[row,col]=255
-                        else:
-                            index_max=slopes.index(m)
-                            chunk_copy[row,col]=d8_directions_dict[index_max]
-                            break
+                        all_same = False
+                        break
+                if all_same:
+                    chunk_copy[row, col] = 255
+                else:
+                    # Get the maximum slope
+                    m = max(slopes)
+                    # If the maximum slope is negative, set the flow direction to 255
+                    if m < 0:
+                        chunk_copy[row, col] = 255
+                    # Otherwise, set the flow direction to the corresponding D8 direction
                     else:
-                        chunk_copy[row,col]=255
-                
-                 
+                        index_max = slopes.index(m)
+                        chunk_copy[row, col] = d8_directions_dict[index_max]
+
     return chunk_copy
 
-def flow_direction_from_chunks(input_path,output_path,chunk_size=1000):
+
+def flow_direction_from_chunks(input_path, output_path, chunk_size=1000):
+    """Generates a flow direction raster from a DEM chunks of a given size."""
     input_raster = gdal.Open(input_path)
-    cell_size=1
-    
+    cell_size = 1.11
     projection = input_raster.GetProjection()
     transform = input_raster.GetGeoTransform()
-    
+
     band = input_raster.GetRasterBand(1)
-    nodata_value=band.GetNoDataValue()
+    nodata_value = band.GetNoDataValue()
     driver = gdal.GetDriverByName("GTiff")
-    dataset = driver.Create(output_path, input_raster.RasterYSize,input_raster.RasterXSize, 1, gdal.GDT_Float32)
-    
+    dataset = driver.Create(
+        output_path,
+        input_raster.RasterYSize,
+        input_raster.RasterXSize,
+        1,
+        gdal.GDT_Float32,
+    )
+
     dataset.SetProjection(projection)
     dataset.SetGeoTransform(transform)
-    output_band=dataset.GetRasterBand(1)
-    
-    for chunk in raster_chunker(band,chunk_size=chunk_size,chunk_buffer_size=2):
-        result= generate_flow_direction_raster(chunk.data,cell_size,nodata_value)
-        chunk.from_numpy(result)
-        chunk.write(output_band) 
-    
-        
+    output_band = dataset.GetRasterBand(1)
 
-chunk = np.array(
-    [   [-999,-999,-999, -999, -999, -999, -999,-999,-999],
-        [-999,-999,-999, -999, -999, -999, -999,-999,-999],
-        [-999,-999,100, 101, 90, 97, 90,-999,-999],
-        [-999, -999,103, 102, 80, 96, 95,-999,-999],
-        [-999, -999,94, 95, 96, 95, 94,-999,-999],
-        [-999, -999,97, 98, 95, 94, 90,-999,-999],
-        [-999, -999,95, 90, 85, 40, 92,-999,-999],
-        [-999,-999,-999, -999, -999, -999, -999,-999,-999],
-        [-999,-999,-999, -999, -999, -999, -999,-999,-999]
-    ]
-    )
-chunk,unsolved=breach_single_cell_pits_in_chunk(chunk,-999)
-print(chunk,unsolved)
-fdir=generate_flow_direction_raster(chunk,1,-999)
-print(fdir)
+    for chunk in raster_chunker(band, chunk_size=chunk_size, chunk_buffer_size=2):
+
+        result = generate_flow_direction_raster(chunk.data, cell_size, nodata_value)
+        chunk.from_numpy(result)
+        chunk.write(output_band)
--- src/overflow_cli.py
+++ src/overflow_cli.py
@@ -1,25 +1,24 @@
-import os
-from pathlib import Path
-from typing import Optional
+
 import click
 
-from breach_single_cell_pits import breach_single_cell_pits
+from overflow.breach_single_cell_pits import breach_single_cell_pits
+from overflow.generate_flow_direction_raster import flow_direction_from_chunks
 
 
 @click.group()
 def main():
     """The main entry point for the command line interface."""
 
+
 @main.command()
 @click.option(
     "--input_file",
     help="path to the DEM file",
 )
-@click.option(
-    "--output_file",
-    help="path to the output file"
-)
-def breach_single_cell_pits(input_file: str, output_file: str,chunk_size: int):
+@click.option("--output_file", help="path to the output file")
+@click.option("--chunk_size", help="chunk size", default=1000)
+
+def breach_single_cell_pits(input_file: str, output_file: str, chunk_size: int):
     """
     This function is used to breach single cell pits in a chunk of a DEM.
     The function takes a chunk of a DEM as input and returns a chunk of DEM with breached single cell pits.
@@ -36,7 +35,41 @@ def breach_single_cell_pits(input_file: str, output_file: str,chunk_size: int):
     None
     """
     try:
-        breach_single_cell_pits(input_file, output_file,chunk_size=2000)
+        breach_single_cell_pits(input_file, output_file, chunk_size)
     except Exception as exc:
-        print(f"breach_single_cell_pits failed with the following exception: {str(exc)}")
+        print(
+            f"breach_single_cell_pits failed with the following exception: {str(exc)}"
+        )
+        raise click.Abort()  # exit with non-zero exit code. Everytime zero is returned on failure a baby kitten dies
+
+@main.command()
+@click.option(
+    "--input_file",
+    help="path to the DEM file",
+)
+@click.option("--output_file", help="path to the output file")
+@click.option("--chunk_size", help="chunk size", default=1000)
+
+def flow_direction_from_chunks(input_file: str, output_file: str, chunk_size: int):
+    """
+    This function is used to generate flow direction rasters from chunks of a DEM.
+    The function takes a chunk of a DEM as input and returns a chunk of DEM with delineated flow direction.
+
+    Parameters
+    ----------
+    input_file : str
+        Path to the input dem file
+    output_file : str
+        Path to the output file
+
+    Returns
+    -------
+    None
+    """
+    try:
+        flow_direction_from_chunks(input_file, output_file, chunk_size)
+    except Exception as exc:
+        print(
+            f"flow_direction_from_chunks failed with the following exception: {str(exc)}"
+        )
         raise click.Abort()  # exit with non-zero exit code. Everytime zero is returned on failure a baby kitten dies
