75d43b8618d11600ca7360859d0daa8e51407ad2
Brendan Barnes <barne856@umn.edu>
2024-03-16 21:47:08 +0000

    add algo 4: label_flats from R. Barnes paper

--- src/overflow/fix_flats.py
+++ src/overflow/fix_flats.py
@@ -59,3 +59,48 @@ def flat_edges(dem: np.ndarray, fdr: np.ndarray) -> tuple[list, list]:
                 high_edges.append((row, col))
                 break
     return high_edges, low_edges
+
+
+def label_flats(
+    dem: np.ndarray, labels: np.ndarray, new_label: int, flat_row: int, flat_col: int
+) -> None:
+    """Algorithm 4 LabelFlats: This flood-fill function gives all the cells of a flat a common label,
+    as described by §2.2. https://rbarnes.org/sci/2014_flats.pdf
+    Upon entry:
+    (1) DEM contains the elevations of every cell or a value NoData for cells not part of the DEM.
+    (2) Labels has the same dimensions as DEM.
+    (3) c belongs to the flat which is to be labeled.
+    (4) L is a unique label which has not been previously applied to a flat.
+    (5) Labels has been initialized to zero prior to the first call to this function.
+    (6) Labels has values greater than or equal to 1 for each processed cell which is in a flat.
+    Each flat's cells bear a label unique to that flat.
+    At exit:
+    (1) c and every cell reachable from c by passing over only cells of the same elevation as c
+    (all the cells in the flat to which c belongs) is marked as L in Labels.
+    (2) Labels has been updated to reflect the new labels which have been applied.
+
+    Args:
+        dem (np.ndarray): The digital elevation model
+        labels (np.ndarray): The labels array
+        new_label (int): The new label to apply to the flat
+        flat_row (int): The initial row of the flat cell
+        flat_col (int): The initial column of the flat cell
+    """
+    # create FIFO queue for to_be_filled
+    to_be_filled = [(flat_row, flat_col)]
+    elev = dem[flat_row, flat_col]
+    while to_be_filled:
+        row, col = to_be_filled.pop(0)
+        not_in_bounds = row < 0 or row >= dem.shape[0] or col < 0 or col >= dem.shape[1]
+        if not_in_bounds:
+            continue
+        if dem[row, col] != elev:
+            continue
+        if labels[row, col] != 0:
+            continue
+        labels[row, col] = new_label
+        # push all 8 neighbors onto the queue
+        for d_row, d_col in NEIGHBOR_OFFSETS:
+            neighbor_row = row + d_row
+            neighbor_col = col + d_col
+            to_be_filled.append((neighbor_row, neighbor_col))
--- tests/test_fix_flats.py
+++ tests/test_fix_flats.py
@@ -1,6 +1,6 @@
 import pytest
 import numpy as np
-from overflow.fix_flats import flat_edges
+from overflow.fix_flats import flat_edges, label_flats
 from overflow.constants import (
     FLOW_DIRECTION_UNDEFINED,
     FLOW_DIRECTION_EAST,
@@ -150,6 +150,27 @@ def fixture_expected_low_edges():
     ]
 
 
+@pytest.fixture(name="expected_flat_labels")
+def fixture_expected_flat_labels():
+    """Create the expected labels for the flat area of the test dem.
+
+    Returns:
+        np.ndarray: A 2D array containing the expected labels for the flat area
+    """
+    return np.array(
+        [
+            [0, 0, 0, 0, 0, 0, 0],
+            [0, 1, 1, 1, 1, 1, 0],
+            [0, 1, 1, 1, 1, 1, 0],
+            [0, 1, 1, 1, 1, 1, 0],
+            [0, 1, 1, 1, 1, 1, 0],
+            [0, 1, 1, 1, 1, 1, 0],
+            [0, 0, 0, 0, 0, 0, 0],
+        ],
+        np.uint32,
+    )
+
+
 def test_flat_edges(dem, fdr, expected_high_edges, expected_low_edges):
     """Test the flat_edges function.
 
@@ -162,3 +183,17 @@ def test_flat_edges(dem, fdr, expected_high_edges, expected_low_edges):
     high_edges, low_edges = flat_edges(dem, fdr)
     assert sorted(high_edges) == sorted(expected_high_edges)
     assert sorted(low_edges) == sorted(expected_low_edges)
+
+
+def test_label_flats(dem, expected_flat_labels):
+    """Test the label_flats function.
+
+    Args:
+        dem (np.ndarray): A dem with a flat that drains
+        expected_flat_labels (np.ndarray): A 2D array containing the expected labels for the flat area
+    """
+    labels = np.zeros(dem.shape, dtype=np.uint32)
+    flat_row, flat_col = 2, 2
+    new_label = 1
+    label_flats(dem, labels, new_label, flat_row, flat_col)
+    assert np.array_equal(labels, expected_flat_labels)
